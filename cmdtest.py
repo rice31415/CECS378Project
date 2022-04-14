d4dd7910ef0955401f08755a67cb0e7b03eb54aa
Eric Wang <ericwang3.1415@gmail.com>
2022-04-14 00:45:05 -0700

    Trying different approach to elevate privilege and disable cmd

--- cmdtest.py
+++ cmdtest.py
@@ -1,10 +1,14 @@
 import winreg
 import ctypes, sys
+import shutil
+import os
+from elevate import elevate
 
 def is_admin():
     try:
         return ctypes.windll.shell32.IsUserAnAdmin()
     except:
+        print("a")
         return False
 
 
@@ -12,18 +16,27 @@ def is_admin():
 path = winreg.HKEY_CURRENT_USER
 
 def create_disablecmd_key():
-    # try:
+    print("c")
+    try:
         key = winreg.OpenKeyEx(path, r"Software\\Policies\\Microsoft\\Windows")
         newKey = winreg.CreateKey(key,"System")
         winreg.SetValueEx(newKey, "DisableCMD", 0, winreg.REG_DWORD, 0)
+        print("a")
         if newKey:
             winreg.CloseKey(newKey)
-    # except Exception as e:
-    #         print(e)
+    except Exception as e:
+            print(e)
 
-if is_admin():
-    create_disablecmd_key()
-else:
-    ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
-    print("idk")
-    create_disablecmd_key()
+#     #doesn't seem to actually escalate to admin
+# if is_admin():
+#     create_disablecmd_key()
+# else:
+#     #ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
+#     os.rename("C:\\Windows\\System32\\cmd.exe", "C:\\Windows\\System32\\cme.exe")
+
+
+elevate() #stuff after it won't run it looks like, maybe needs main function
+# print("b")
+# create_disablecmd_key()
+
+#shutil.move("C:\\cmd.exe", "C:\\Windows\\System32\\cmd.exe")
